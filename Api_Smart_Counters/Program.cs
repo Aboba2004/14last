41ffc7c4eed854a9979c167c9a303f82787140e1
Arefiev Yuriy Vladimirovich <delurya22@gmail.com>
2024-04-02 20:31:59 +0300

    ver1

--- Api_Smart_Counters/Program.cs
+++ Api_Smart_Counters/Program.cs
@@ -1,28 +1,53 @@
 using Microsoft.EntityFrameworkCore;
+using NLog;
+using NLog.Web;
+using Microsoft.Extensions.Logging;
 
-var builder = WebApplication.CreateBuilder(args);
+var logger = NLog.LogManager.Setup().LoadConfigurationFromAppSettings().GetCurrentClassLogger();
+logger.Debug("init main");
 
-// Add services to the container.
-
-builder.Services.AddControllersWithViews();
-builder.Services.AddDbContext<Api_Smart_Counters.Models.Smart_CountersContext>(option => option.UseSqlServer(builder.Configuration.GetConnectionString("con")));
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+try
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    var builder = WebApplication.CreateBuilder(args);
+
+    // NLog: configure NLog for Dependency injection
+    builder.Logging.ClearProviders();
+    builder.Logging.SetMinimumLevel(Microsoft.Extensions.Logging.LogLevel.Trace);
+    builder.Host.UseNLog();
+
+    // Add services to the container.
+    builder.Services.AddControllersWithViews();
+    // Настройте контекст базы данных так, как вы это делаете в своем приложении
+    builder.Services.AddDbContext<Api_Smart_Counters.Models.Smart_CountersContext>(option =>
+        option.UseSqlServer(builder.Configuration.GetConnectionString("con")));
+    builder.Services.AddEndpointsApiExplorer();
+    builder.Services.AddSwaggerGen();
+
+    var app = builder.Build();
+
+    // Configure the HTTP request pipeline.
+    if (app.Environment.IsDevelopment())
+    {
+        app.UseSwagger();
+        app.UseSwaggerUI();
+    }
+
+    app.UseHttpsRedirection();
+
+    app.UseAuthorization();
+
+    app.MapControllers();
+
+    app.Run();
 }
-
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
-
-app.MapControllers();
-
-app.Run();
+catch (Exception exception)
+{
+    // NLog: catch setup errors
+    logger.Error(exception, "Stopped program because of exception");
+    throw;
+}
+finally
+{
+    // Ensure to flush and stop internal timers/threads before application-exit (Avoid segmentation fault on Linux)
+    NLog.LogManager.Shutdown();
+}
